4ca892a7b7855c81467e1a8bb4fa5d4e21e67c6c
Fernando Michnowicz <fmichnowicz@fi.uba.ar>
2025-11-17 20:06:06 -0300

    Agregado de la sección FAQ y código JS para funcionalidad

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,28 @@
-// Menu en dispositivos móbiles
+//Menu hamburguesa en dispositivos móbiles
 const burgerIcon = document.querySelector('#burger');
 const navBarMenu = document.querySelector('#nav-links');
 
 burgerIcon.addEventListener('click', () => {
     burgerIcon.classList.toggle('is-active');
     navBarMenu.classList.toggle('is-active');
+});
+
+//Menú desplegable en sección preguntas frecuentes
+document.addEventListener('DOMContentLoaded', () => {
+    const headings = document.querySelectorAll('.faq-panel .panel-heading');
+
+    headings.forEach(heading => {
+        heading.addEventListener('click', () => {
+            const block = heading.nextElementSibling;      // el .panel-block
+            const arrow = heading.querySelector('.arrow'); // la flechita
+
+            // Alternar visibilidad
+            block.classList.toggle('is-hidden');
+
+            // Cambiar estado aria y flecha
+            const isExpanded = !block.classList.contains('is-hidden');
+            heading.setAttribute('aria-expanded', isExpanded);
+            arrow.textContent = isExpanded ? '▲' : '▼';
+        });
+    });
 });
