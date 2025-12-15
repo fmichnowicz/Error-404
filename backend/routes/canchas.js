5f2c2ee8cdcd6f3e179e49978b1268f93efe7469
Fernando Michnowicz <fmichnowicz@fi.uba.ar>
2025-12-15 03:40:17 -0300

    creación de base de datos, docker compose, fusión de models y controllers, creación de index para rutas

--- backend/routes/canchas.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import canchaController from '../controllers/canchas.js';
-
-router.get('/', canchaController.obtenerTodas);
-
-router.get('/:id', canchaController.obtenerPorId);
-
-export default router;
